924820e8ad9b78f54778e51bf5c55136c396b87c
ceeker <409273291@qq.com>
2018-04-23 17:47:12 +0800

    增加数据管理器

---
+++ src/main/java/aqi/DataManger.java
@@ -0,0 +1,15 @@
+package aqi;
+
+import java.util.List;
+
+/**
+ * @author vectorzhang
+ * @desc 数据管理
+ * @date 2018/4/23 17:45
+ */
+public class DataManger {
+
+    public List<String> getDataFiles(String dataPath) {
+        return null;
+    }
+}
